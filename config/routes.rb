84d2589a668621747d97e7af25ab3d6d48730764
Jack de Grave <jack.degrave@outlook.com>
2019-11-25 13:09:34 +0000

    backend user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   resources :likes
   resources :posts
   resources :users
-
   post '/login', to: 'auth#create'
   get '/validate', to: 'auth#validate_token'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user_1 = User.create(username: 'jack', password: 'password')
-
-post_1 = Post.create(description: 'red blood moon', category: 'moon', user_id: 1, image: 'https://cdn.mos.cms.futurecdn.net/Kt5mJx5MFKp5x7jz5geHwn-970-80.jpg')
-
 puts 'database seeded'
