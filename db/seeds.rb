defc148130c9574a31da4760f11adc94ad64c9ba
Jack de Grave <jack.degrave@outlook.com>
2019-11-26 23:54:59 +0000

    seeds v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+post_1 = Post.create(description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." , category: "space", user_id: 1, image: "astronaught_1.jpeg")
+post_2 = Post.create(description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", category: "earth", user_id: 2, image: "")
+post_3 = Post.create(description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", category: "astronaught", user_id: 3, image: "")
+post_4 = Post.create(description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", category: "planet", user_id: 4, image: "")
+post_5 = Post.create(description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", category: "stars", user_id: 5, image: "")
+post_6 = Post.create(description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", category: "event", user_id: 6, image: "")
+
 puts 'database seeded'
